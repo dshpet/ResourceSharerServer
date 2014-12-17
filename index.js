b61500b83855172c726868d2bd61a0be942e98ef
dshpet <dshpet@gmail.com>
2014-12-17 14:29:46 +0200

    AddedDB require

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
 
+//get and connect to db
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://admin:admin@ds062097.mongolab.com:62097/resource-sharer-statistics');
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
