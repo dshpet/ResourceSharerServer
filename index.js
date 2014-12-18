0694b1b22682c7ea4b08499fef2c6dd0e1d58755
dshpet <dshpet@gmail.com>
2014-12-18 12:07:53 +0200

    some fixes

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var app = express();
 var cool = require('cool-ascii-faces');
 
 //get and connect to db
+var dbUrl = 'mongodb://admin:admin@ds062097.mongolab.com:62097/resource-sharer-statistics';
+var collections = null; //if only some collections are needed
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://admin:admin@ds062097.mongolab.com:62097/resource-sharer-statistics');
+mongoose.connect(dbUrl);
 
 //set up db context
 var db = mongoose.connection;
@@ -46,16 +48,24 @@ app.use(express.static(__dirname + '/public'));
 app.set('title', 'ResourceSharer gathering');
 
 app.get('/', function(request, response) {
-    response.send(cool());
+    response.send(cool() + 'Get out of here. This is a database server' + cool());
 });
+
 app.get('/dbUrl', function(request, response){
-    response.send('mongodb://admin:admin@ds062097.mongolab.com:62097/resource-sharer-statistics');
+    response.send(dbUrl);
 });
+
 app.get('/recieveData', function(request, response){
     response.send("WOW");
 });
+//request must be a json string
 app.post('/recieveData', function(request, response){
-    response.send("WOW");
+    request.on('end', function(){
+        var object = JSON.parse(request);
+        object.save(function (err, object){
+            if (err) return console.error(err);
+        });
+    });
 });
 
 app.listen(app.get('port'), function() {
