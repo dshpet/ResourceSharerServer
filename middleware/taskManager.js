d6ae32ba14594413fbfd34b1b42920956fea32ec
dshpet <dshpet@gmail.com>
2014-12-21 22:20:13 +0200

    Added taskManager and utils 3

--- middleware/taskManager.js
+++ middleware/taskManager.js
@@ -1,11 +1,19 @@
 var databaseManager = require('./databaseManager');
 
 module.exports = {
-    getAllTasks: function (res) {
+    getAllTasksReadable: function (res) {
         var modelSchema = databaseManager.computationalDataSchema;
         var model =  databaseManager.model("ComputationalData", modelSchema);
 
 
         var alltasks = databaseManager.getTasks(res);
+    },
+    getAllTasks: function (res){
+        var db = databaseManager.connection;
+        db.collection('computationaltasks', function(err, collection){
+            collection.find().toArray(function(err, items) {
+                res.send(items);
+            });
+        });
     }
 };
