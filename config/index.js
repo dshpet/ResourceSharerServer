301815a4124880d076f13854a5fe1d637edd86f4
dshpet <dshpet@gmail.com>
2014-12-21 13:56:17 +0200

    another split and addition

--- config/index.js
+++ config/index.js
@@ -66,7 +66,51 @@ app.get('/dbUrl', function(request, response){
 });
 
 
-var dataReceiver = require('./dataReceiver.js');
+
+app.get('/recieveData', function(request, response){
+
+    /*var partedObject = "";
+
+     request.on('data', function(data){
+     partedObject += data;
+     });*/
+
+    response.send("Place to send data");
+});
+//request must be a json string
+app.post('/recieveData', function(request, response){
+    var object = null;
+    var partedObject = "";
+
+    response.send("JUST SOME RESPONSE TEXT");
+
+    request.on('data', function(data){
+        partedObject += data;
+    });
+
+    request.on('end', function(){
+        console.log("POST method on end : " + request);
+
+        //object = JSON.parse(request);
+
+        var b = request.body;
+        var h = request.headers;
+        console.log("body : " + b);
+
+        //response.send("Received request: " + request + " parsed object: " + object + "\n Parted string received: " + partedObject);
+        //response.send("Parted string received: " + partedObject);
+        response.json({"json" : "test"});
+
+        /*object.save(function (err, object){
+         if (err) return console.error(err);
+         });*/
+    });
+
+    request.on('error', function (e) {
+        response.send("Error occurred" + e);
+    });
+});
+var dataReceiver = require('./../modules/dataReceiver.js');
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
